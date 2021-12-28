4d8b07e62684144e3ebd3805809826566ab4313e
Jeremy <jeremytbaxter@protonmail.com>
2021-12-28 14:17:23 +1300

    Added a new colour scheme (looks bad by itself, fits my bg) + changed terminal emulator to Alacritty + added new shortcut for web browser: mod + e (default = librewolf)

--- config.h
+++ config.h
@@ -13,7 +13,7 @@ static const char col_gray3[]       = "#bbbbbb";
 /* was #eeeeee */
 /* col_cyan used to be #2644ba */
 static const char col_gray4[]       = "#ffffff";
-static const char col_cyan[]        = "#FF5900";
+static const char col_cyan[]        = "#1ba4f5";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -59,12 +59,16 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+/* My addition, web browser variable so it can be spawned using mod + e */
+static const char *wbrsrcmd[] = { "librewolf", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_x,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	/* allows for spawning web browser */
+	{ MODKEY,                       XK_e,      spawn,          {.v = wbrsrcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
