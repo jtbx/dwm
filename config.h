57d615091488ff7c1057c54e49ce8e168ea501e3
Jeremy <jeremytbaxter@protonmail.com>
2021-12-29 07:08:58 +1300

    Changed the colour scheme to white on black, after failing to add transparency

--- config.h
+++ config.h
@@ -12,8 +12,8 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 /* was #eeeeee */
 /* col_cyan used to be #2644ba */
-static const char col_gray4[]       = "#ffffff";
-static const char col_cyan[]        = "#1a7ddb";
+static const char col_gray4[]       = "#222222";
+static const char col_cyan[]        = "#ffffff";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
