f11abf862a70d54714fae828601c7a147c2845b7
Jeremy Baxter <jeremytbaxter@protonmail.com>
2022-01-01 17:33:38 +1300

    Updated config.def.h

--- config.def.h
+++ config.def.h
@@ -2,16 +2,19 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 10; 	/* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "JetBrainsMono Nerd Font-Regular:size=10" };
+static const char dmenufont[]       = "JetBrainsMono-Regular:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+/* was #eeeeee */
+/* col_cyan used to be #2644ba */
+static const char col_gray4[]       = "#222222";
+static const char col_cyan[]        = "#ffffff";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -56,44 +59,71 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+/* Dmenu command remapped to rofi */
+static const char *dmenucmd[] = { "rofi", "-show", "run", NULL };
+/* Rofi application menu variable so it can be spawned using mod + x */ 
+static const char *appmenucmd[] = { "rofi", "-show", "drun", NULL };
+/* Terminal command remapped to alacritty */
+static const char *termcmd[]  = { "alacritty", NULL };
+/* Web browser variable so it can be spawned using mod + e */
+static const char *wbrsrcmd[] = { "librewolf", NULL };
+ /* Rofi emoji menu variable so it can be spawned using mod + period */
+/* requires rofi-emoji                                              */
+static const char *emojicmd[] = { "rofi", "-show", "emoji", NULL };
+/* Power off variable so it can be run using mod + shift + escape */
+static const char *powercmd[] = { "poweroff", NULL };
+/* Reboot variable so it can be run using mod + shift + r */
+static const char *rebootcmd[] = { "reboot", NULL };
+
+/* For powercmd & rebootcmd I'm planning for it to open a terminal window where it would ask you to confirm the poweroff/reboot. */
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	/* modifier                  key           function        argument */
+	{ MODKEY,                    XK_p,         spawn,          {.v = dmenucmd } },
+	{ MODKEY,                    XK_x,         spawn,          {.v = appmenucmd } },
+	{ MODKEY,                    XK_Return,    spawn,          {.v = termcmd } },
+	/* allows for spawning web browser */
+	{ MODKEY,                    XK_w,         spawn,          {.v = wbrsrcmd } },
+	/* allows for spawning emoji menu */
+	{ MODKEY,                    XK_semicolon, spawn,          {.v = emojicmd } },
+	/* allows for turning off computer */
+	{ MODKEY|ShiftMask,          XK_Escape,    spawn,          {.v = powercmd } },
+	/* allows for rebooting computer */
+	{ MODKEY|ShiftMask,          XK_r,         spawn,          {.v = rebootcmd } },
+	{ MODKEY,                    XK_b,         togglebar,      {0} },
+	{ MODKEY,                    XK_j,         focusstack,     {.i = +1 } },
+	{ MODKEY,                    XK_k,         focusstack,     {.i = -1 } },
+	{ MODKEY,                    XK_i,         incnmaster,     {.i = +1 } },
+	{ MODKEY,                    XK_d,         incnmaster,     {.i = -1 } },
+	{ MODKEY,                    XK_h,         setmfact,       {.f = -0.05} },
+	{ MODKEY,                    XK_l,         setmfact,       {.f = +0.05} },
+	{ MODKEY,                    XK_Return,    zoom,           {0} },
+	{ MODKEY,                    XK_Tab,       view,           {0} },
+	{ MODKEY,                    XK_q,         killclient,     {0} },
+	{ MODKEY,                    XK_t,         setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                    XK_f,         setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                    XK_m,         setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                    XK_space,     setlayout,      {0} },
+	{ MODKEY|ShiftMask,          XK_space,     togglefloating, {0} },
+	{ MODKEY,                    XK_0,         view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,          XK_0,         tag,            {.ui = ~0 } },
+	{ MODKEY,                    XK_comma,     focusmon,       {.i = -1 } },
+	{ MODKEY,                    XK_period,    focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,          XK_comma,     tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,          XK_period,    tagmon,         {.i = +1 } },
+	{ MODKEY,                    XK_minus,     setgaps,        {.i = -1 } },
+	{ MODKEY,                    XK_equal,     setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,          XK_equal,     setgaps,        {.i = 0  } },
+	TAGKEYS(                     XK_1,                         0)
+	TAGKEYS(                     XK_2,                         1)
+	TAGKEYS(                     XK_3,                         2)
+	TAGKEYS(                     XK_4,                         3)
+	TAGKEYS(                     XK_5,                         4)
+	TAGKEYS(                     XK_6,                         5)
+	TAGKEYS(                     XK_7,                         6)
+	TAGKEYS(                     XK_8,                         7)
+	TAGKEYS(                     XK_9,                         8)
+	{ MODKEY,                    XK_Escape,    quit,           {0} },
 };
 
 /* button definitions */
