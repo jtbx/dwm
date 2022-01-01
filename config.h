4d063f8d7f4b115db179325995af0243d316e0f7
Jeremy Baxter <jeremytbaxter@protonmail.com>
2022-01-01 17:24:29 +1300

    Forgot what this was

--- config.h
+++ config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 10; 	/* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono-Regular:size=10" };
+static const char *fonts[]          = { "JetBrainsMono Nerd Font-Regular:size=10" };
 static const char dmenufont[]       = "JetBrainsMono-Regular:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
