d85d8e040f988b120095eb434a9f7a51fd6a5950
Jeremy <jeremytbaxter@protonmail.com>
2022-02-17 23:22:20 +1300

    Changed colour scheme (to match my vim colourscheme), the size of text, the bar, added .ccls file

--- config.def.h
+++ config.def.h
@@ -17,7 +17,7 @@ static const char col_gray3[]       = "#bbbbbb";
 /* was #eeeeee */
 /* col_cyan used to be #2644ba */
 static const char col_gray4[]       = "#222222";
-static const char col_cyan[]        = "#ffffff";
+static const char col_cyan[]        = "#0095e5";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
